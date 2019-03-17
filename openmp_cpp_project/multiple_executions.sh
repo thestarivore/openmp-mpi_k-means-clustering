549f07ef36a424aa0ebe793451440ad4f3a13a8d
starivore <starivore@tutamail.com>
2019-03-17 11:31:19 +0000

    Completed Results run on Amazon AWS Cluster for the 1M Dataset

--- openmp_cpp_project/multiple_executions.sh
+++ openmp_cpp_project/multiple_executions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for i in 1 2 3 4 5 6 7 8 9 10
+for i in 9 10
 do
-    for j in 1 2 3 4 5 6 7 8 9 10
+    for j in 1 2 3 4 5 6 7
     do
 	   #mpiexec -n 4 bin/Debug/openmp_cpp_project $i
             mpiexec -np 4 -hostfile /home/sgeadmin/hostfile ./main $i
