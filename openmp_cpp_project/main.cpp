3ecf3839ae6d31b743ca544a943aa1906704ee40
starivore <starivore@tutamail.com>
2019-04-13 20:38:18 +0000

    Added New Results run on Amazon AWS Cluster for the 100,1K,10K,100K and 1M Datasets

--- openmp_cpp_project/main.cpp
+++ openmp_cpp_project/main.cpp
@@ -57,7 +57,7 @@ int numtasks, rank;
 MPI_Datatype mpi_point_type;
 
 int main(int argc, char *argv[]) {
-    char datasetFile[]          = "../dataset_display/dataset_100K.csv";
+    char datasetFile[]          = "../dataset_display/dataset_1M.csv";
     char initialDatasetFile[]   = "../dataset_display/initialdataset.csv";
     char initialCentroidsFile[] = "../dataset_display/initialcentroids.csv";
     char newDatasetFile[]       = "../dataset_display/newdataset.csv";
@@ -222,7 +222,7 @@ KMCResult kMeansClustering(int k, int n_rows, char newDatasetFile[], char newCen
     float J;
 
     //Start time measurement
-    printf("Start time measurement\n");
+    //printf("Start time measurement\n");
     if(rank == 0)
         start = omp_get_wtime();
 
